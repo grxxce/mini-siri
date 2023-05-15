32daaca05126fcde38ede49f52439298ec6a8e35
Grace Li <graceli.contact@gmail.com>
2023-05-15 16:38:39 -0400

    working app, no styling

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/css/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+// import './assets/css/main.css';
 
-createApp(App).mount('#app')
+// import Vuesax from 'vuesax';
+// import 'vuesax/dist/vuesax.css'; // Import Vuesax styles
+
+const app = createApp(App);
+// app.use(Vuesax);
+app.mount('#app');
