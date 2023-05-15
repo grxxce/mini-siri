398fac9a1de037889f605e03a4d66e3e2d233450
Grace Li <grace.l5678t@gmail.com>
2023-05-15 18:02:49 -0400

    fully styled version

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-// import './assets/css/main.css';
-
-// import Vuesax from 'vuesax';
-// import 'vuesax/dist/vuesax.css'; // Import Vuesax styles
 
 const app = createApp(App);
-// app.use(Vuesax);
 app.mount('#app');
